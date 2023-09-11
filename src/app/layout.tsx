2a25a3ef722ee046e0bff3fc1733847d378aaa94
alex 3 <alex@alexavfrantz.com>
2023-09-11 01:05:29 -0400

    remove metadata for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,6 @@ import type { Metadata } from 'next';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
-export const metadata: Metadata = {
-  title: "Alex's Photo Mode Shots",
-  description: "All (or, well, most) of the shots I've taken in various games",
-}
-
 export default function RootLayout({
   children
 }: {
